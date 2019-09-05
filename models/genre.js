e0ec1d729f943a861d9449bebcc1166b1d091693
vitor carvalho <vitordevir@gmail.com>
2019-09-05 13:50:22 -0300

    First version of application moved into github

---
+++ models/genre.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+{
+    name:{type: String, required: true, min: 3 , max: 100}
+}
+);
+//virtual for GenreSchema's URL
+GenreSchema
+.virtual('url')
+.get(function(){return '/catalog/genre/' + this._id; });
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
